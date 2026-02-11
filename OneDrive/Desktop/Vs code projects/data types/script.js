0892078f4b48f5183defe5b09f67cdc9cc39d986
umarkulov-s <sonwesley79@gmail.com>
2026-02-11 22:23:51 +0500

    nw ch

---
+++ OneDrive/Desktop/Vs code projects/data types/script.js
@@ -0,0 +1,15 @@
+// var a = prompt("Matn yozing").trim();
+// console.log(a.split(a.charAt(a.length-1; 
+
+var a = prompt("soz kiriting").trim();
+
+if (a) {
+    if(a[0] == a[0].toLowerCase()){
+        alert(a[0].toUpperCase() + a.slice(1))
+    }
+    else {
+        alert(a[0].toLowerCase() + a.slice(1));
+    }
+}
+// var res = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
+// alert(res);
