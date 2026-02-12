269ebc1adbb96b8beda61ebfe604059ce384693e
umarkulov-s <sonwesley79@gmail.com>
2026-02-12 16:15:06 +0500

    hw

--- OneDrive/Desktop/Vs code projects/data types/script.js
+++ OneDrive/Desktop/Vs code projects/data types/script.js
@@ -1,15 +1,13 @@
-// var a = prompt("Matn yozing").trim();
-// console.log(a.split(a.charAt(a.length-1; 
+var name = prompt("Введите свое имя :").trim();
 
-var a = prompt("soz kiriting").trim();
-
-if (a) {
-    if(a[0] == a[0].toLowerCase()){
-        alert(a[0].toUpperCase() + a.slice(1))
+if (name) {
+    if (name[0] == name[0].toLowerCase()){
+        alert(name[0].toUpperCase() + name.slice(1))
     }
     else {
-        alert(a[0].toLowerCase() + a.slice(1));
+        alert(name[0].toLowerCase() + name.slice(1));
     }
 }
-// var res = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
-// alert(res);
+else {
+    alert("Введено неверное имя")
+}
