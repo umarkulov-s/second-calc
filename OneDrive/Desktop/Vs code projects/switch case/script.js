b5af5420324299a743d7b9c00076478791317007
umarkulov-s <sonwesley79@gmail.com>
2026-02-18 22:10:07 +0500

    gtr

---
+++ OneDrive/Desktop/Vs code projects/switch case/script.js
@@ -0,0 +1,67 @@
+// var a = +prompt("son").trim();
+// var amallar = prompt("amal").trim();
+// var b = +prompt("son").trim();
+
+// var resultat;
+
+// switch (amallar) {
+//     case "+":
+//         console.log(resultat = a + b);
+//         break;
+//     case  "-":
+//         console.log(resultat = a - b);
+//         break;
+//     case "*":
+//         console.log(resultat = a * b);
+//         break;
+//     case  "/":
+//         console.log(resultat = a / b);
+//         if (b == "0") {
+//             console.log("0ga bolinmaydi");
+//         }
+//         break;
+        
+
+//     default:
+//         console.log("Notog'ri amal kiritildi");
+//         break;
+    
+// }
+
+
+
+// var amal = prompt("Amalni yozing").trim().toLowerCase();
+// alert(eval(amal));
+
+var amal =  prompt("Amalni yozing ").trim();
+
+var res = amal.split(" ");
+
+var a = Number(res[0]);
+var oper = res[1];
+var b = Number(res[2]);
+
+switch (oper) {
+    case "+":
+        alert(a + b);
+        break;
+
+    case "-":
+        alert(a - b);
+        break;
+
+    case "*":
+        alert(a * b);
+        break;
+
+    case "/":
+        if (b === 0) {
+            alert("0 ga bo'linmaydi");
+        } else {
+            alert(a / b);
+        }
+        break;
+
+    default:
+        alert("Notogri amal kiritildi");
+}
